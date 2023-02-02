5337e5f37a0689e3454ac04313db1f1d5fb82302
nnnLik <makhmudov_ra@vk.com>
2023-02-02 17:17:11 +0300

    [FIX] framerate data handling

--- service/test.py
+++ service/test.py
@@ -3,6 +3,6 @@ import asyncio
 
 async def heavy_data_processing(data: dict):
     """Some (fake) heavy data processing logic."""
-    await asyncio.sleep(2)
-    message_processed = data.get("message", "").upper()
+    print(data)
+    message_processed = data.get("message", data)
     return message_processed
